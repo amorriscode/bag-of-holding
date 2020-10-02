5b925032f5ba47525fe530c50793e6093474ad5d
Anthony Morris <anthony@amorrissound.com>
2020-10-02 16:45:26 -0700

    Hook up logic for ability scores

--- src/components/AbilityScore.tsx
+++ src/components/AbilityScore.tsx
@@ -8,27 +8,35 @@ import {
 import NumberInput from './NumberInput'
 
 type Props = {
-  label: string
+  ability: string
 }
 
-export default function AbilityScore({ label }: Props) {
+const getModifier = (score: number) => {
+  const modifier = Math.floor((score - 10) / 2)
+  return modifier > 0 ? `+${modifier}` : `${modifier}`
+}
+
+export default function AbilityScore({ ability }: Props) {
   const { abilityScores } = useCharacterSheet()
   const characterSheetDispatch = useCharacterSheetDispatch()
 
+  const abilityScore = abilityScores[ability.toLowerCase()]
+  const modifier = getModifier(abilityScore)
+
   return (
     <div className="bg-white shadow text-center rounded p-2 space-y-2">
-      <div className="uppercase text-xxs font-bold truncate">{label}</div>
+      <div className="uppercase text-xxs font-bold truncate">{ability}</div>
 
-      <div className="font-bold text-xl">+4</div>
+      <div className="font-bold text-xl">{modifier}</div>
 
       <NumberInput
-        name={label}
-        value={abilityScores[label]}
+        name={ability}
+        value={abilityScore || 0}
         onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
           characterSheetDispatch({
             type: 'abilityScore',
             data: {
-              ability: label.toLowerCase(),
+              ability: ability.toLowerCase(),
               score: parseInt(e.target.value),
             },
           })
